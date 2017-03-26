22667e3096753971f8c29d84ec53bf01dd178cfa
Alberto Miranda <codealchemist@gmail.com>
2017-03-26 03:40:58 -0300

    style fixes; removed unused code.

--- pages/player.js
+++ pages/player.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
 import 'isomorphic-fetch'
 import Button from 'react-md/lib/Buttons/Button'
 import FontIcon from 'react-md/lib/FontIcons'
 import TextField from 'react-md/lib/TextFields'
-import Dialog from 'react-md/lib/Dialogs'
 import Toolbar from 'react-md/lib/Toolbars'
 import Page from '../layouts/default'
 
@@ -23,11 +21,11 @@ export default class Player extends React.Component {
     this.state.saveButton = this.getSaveButton()
 
     this.nav = <Button icon onClick={() => this.closeEditor()}>close</Button>
-    this.action = <Button flat label="Save" onClick={() => this.closeEditor()} />
+    this.action = <Button flat label='Save' onClick={() => this.closeEditor()} />
   }
 
   getInitialState () {
-    return JSON.parse(localStorage.getItem('Player') || '{}');
+    return JSON.parse(localStorage.getItem('Player') || '{}')
   }
 
   restore () {
@@ -38,7 +36,7 @@ export default class Player extends React.Component {
   }
 
   saveState () {
-    localStorage.setItem('Player', JSON.stringify(this.state));
+    localStorage.setItem('Player', JSON.stringify(this.state))
   }
 
   static getInitialProps ({ res, xhr }) {
@@ -50,7 +48,7 @@ export default class Player extends React.Component {
     return this.props.statusCode !== null
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.initEditor()
     this.restore()
   }
@@ -94,21 +92,21 @@ export default class Player extends React.Component {
   getSaveButton () {
     if (this.state.name && this.state.code) {
       return (
-        <Button raised primary label="Save" onClick={() => this.save()}>
+        <Button raised primary label='Save' onClick={() => this.save()}>
           <FontIcon>save</FontIcon>
         </Button>
       )
     }
 
     return (
-      <Button raised disabled label="Save" onClick={() => this.save()}>
+      <Button raised disabled label='Save' onClick={() => this.save()}>
         <FontIcon>save</FontIcon>
       </Button>
     )
   }
 
   updateName (name) {
-    this.state.name = name.trim().replace(/[ =\/\\\?|`'"<>]/, '')
+    this.state.name = name.trim().replace(/[ =\/\\\?|`'"<>]/, '') // eslint-disable-line
     this.state.saveButton = this.getSaveButton()
     this.setState(this.state)
     this.saveState()
@@ -127,27 +125,27 @@ export default class Player extends React.Component {
             nav={this.nav}
             actions={this.action}
             style={{zIndex: 300}}
-            title="Code Editor"
+            title='Code Editor'
             fixed
           />
-          <div id="editor"></div>
+          <div id='editor' />
         </div>
 
         <Page>
           <h2>Player</h2>
 
           <TextField
-            id="floatingCenterTitle"
-            label="Name"
+            id='floatingCenterTitle'
+            label='Name'
             style={{margin: 'auto'}}
-            lineDirection="center"
+            lineDirection='center'
             placeholder="Your player's name"
-            className="md-cell md-cell--bottom"
+            className='md-cell md-cell--bottom'
             value={this.state.name}
             onChange={(value) => this.updateName(value)}
           />
 
-          <Button raised secondary label="Code Editor" onClick={() => this.openEditor()}>
+          <Button raised secondary label='Code Editor' onClick={() => this.openEditor()}>
             <FontIcon>code</FontIcon>
           </Button>
 
